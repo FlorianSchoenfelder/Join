06dea0aa8cc12125e34996ce457726a95ef307a3
FlorianSchoenfelder <f.schoenfelder22@gmail.com>
2024-01-15 20:08:27 +0100

    add contact card halfway done

--- JavaScript/contact.js
+++ JavaScript/contact.js
@@ -2,21 +2,22 @@ function stopPropagation(event) {
     event.stopPropagation();
 }
 
-function openAddContactPopup() {
-    document.getElementById('darkBackgroundContainer').classList.add('darkBackground');
-    document.getElementById('darkBackgroundContainer').classList.remove('d-none');
-    document.getElementById('addContactPopup').style = 'transform: translateX(0)';
-}
+// function openAddContactPopup() {
+//     document.getElementById('darkBackgroundContainer').classList.remove('swipeOut');
+//     document.getElementById('darkBackgroundContainer').classList.add('darkBackground'); //Hintergrundfarbe grau hinzufügen
+//     document.getElementById('darkBackgroundContainer').classList.remove('d-none'); // Container sichtbar machen
+//     document.getElementById('addContactPopup').style = 'transform: translateX(0)'; // Popup hereinswipen
+// }
+
+// function closeAddContactPopup() {
+//     document.getElementById('addContactPopup').style = 'transform: translateX(100%)'; // Popup herausswipen
+//     document.getElementById('darkBackgroundContainer').classList.add('swipeOut');
+//     setTimeout(myStopFunction, 250);
+// }
 
 
-function closeAddContactPopup() {
-    setTimeout(() => myStopFunction(), 250)
-    document.getElementById('addContactPopup').style = 'transform: translateX(100%)';
-}
 
-
-
-function myStopFunction() {
-    document.getElementById('darkBackgroundContainer').classList.add('d-none');
-    document.getElementById('darkBackgroundContainer').classList.remove('darkBackground');
-}
+// function myStopFunction() {
+//     document.getElementById('darkBackgroundContainer').classList.add('d-none');
+//     document.getElementById('darkBackgroundContainer').classList.remove('darkBackground');
+// }
